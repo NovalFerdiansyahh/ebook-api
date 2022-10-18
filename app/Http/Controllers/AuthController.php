0402b59da43fe072e96682b43b878cf710937447
NovalFerdiansyahh <91859088+NovalFerdiansyahh@users.noreply.github.com>
2022-10-18 10:51:19 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+
+
+{
+    public static function me (){
+    return [
+        'NIS' => 3103120168,
+        'Name' => 'Noval F',
+        'Phone' => '081215318009',
+        'Class' => 'XII RPL 5'
+    ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//string
+Route::get('coba', function () {
+    return view('hai');
+});
+//array
+Route::get('coba1', function () {
+    return ['petra', 'mus', 'starry'];
+});
+//objek json
+Route::get('coba2', function () {
+    return[
+        'Nama' => 'Noval F',
+        'Kelas'  => 'XII RPL 5',
+        'NIS' => 3103120168
+    ];
+});
+//objek json
+Route::get('coba3', function () {
+    return response()->json(
+        [
+            'Nama' => 'Noval F',
+            'Kelas' => 'XII RPL 5',
+            'NIS' => 3103120168
+        ],201
+        );
+});
+
